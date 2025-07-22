3e81d4d76b4f48455806359f6cacd9d3d159fdc0
Sayali Pawar <sayalipawar20055@gmail.com>
2025-07-22 08:47:38 +0530

    js started

---
+++ JavaScript/app.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+console.log("I'm Beautiful");
+
+let pencilPrice = 10;
+let eraserPrice = 5;
+console.log("The total price is : ",pencilPrice + eraserPrice, "Rupees. ");
+
+//Template Literals
+let a=5;
+let b=10;
+console.log(`You pay ${a + b} rupees`);
